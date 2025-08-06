34567aa413575c3fa5823f41830114e8684367f2
Santhosh <santhoshkumarkarthikeyan5@gmail.com>
2025-08-06 22:32:27 +0530

    setup strategy for voting mechanisms

---
+++ src/knn/voiting/strategy.py
@@ -0,0 +1,17 @@
+from abc import ABC, abstractmethod
+from pandas import DataFrame, Series
+
+class VotingStrategy(ABC):
+    """Class description:
+        Strategy class that is abstract and expects a concrete strategy, either 
+        weighted or unweighted voting to be implemented in the following getRank()
+        method
+    """
+    @abstractmethod
+    def getRank(self, distance: Series) -> Series:
+        """
+        Function description:
+        Method that based on the approach, will calculate the rank for a series of distance provided.
+        """
+        raise NotImplementedError()
+    
